4ccd217cf053930ac40dc8fc2adfbf07a9de9d75
Suprim Khatri <supremekhatri62@gmail.com>
2024-09-09 22:52:27 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+const courseCon = document.querySelectorAll('#courseCon');
+
+courseCon.forEach((courseCon) => {    
+    const video = courseCon.querySelector('#myVideo');
+    courseCon.addEventListener('mouseenter', () => {
+      video.play();
+    });
+
+    courseCon.addEventListener('mouseleave', () => {
+        const video = courseCon.querySelector('#myVideo');
+        video.pause();
+        video.currentTime = 0; 
+      });
+    });
