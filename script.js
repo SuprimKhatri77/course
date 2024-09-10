535352921d7fd332fbc33b1a83b0e8324b0d9131
Suprim Khatri <supremekhatri62@gmail.com>
2024-09-10 15:20:25 +0545

    minor error fixes

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
+const courseCon = document.querySelectorAll("#courseCon");
 
-const courseCon = document.querySelectorAll('#courseCon');
+courseCon.forEach((courseCon) => {
+  const video = courseCon.querySelector("#myVideo");
+  courseCon.addEventListener("mouseenter", () => {
+    video.play();
+  });
 
-courseCon.forEach((courseCon) => {    
-    const video = courseCon.querySelector('#myVideo');
-    courseCon.addEventListener('mouseenter', () => {
-      video.play();
-    });
-
-    courseCon.addEventListener('mouseleave', () => {
-        const video = courseCon.querySelector('#myVideo');
-        video.pause();
-        video.currentTime = 0; 
-      });
-    });
+  courseCon.addEventListener("mouseleave", () => {
+    const video = courseCon.querySelector("#myVideo");
+    video.pause();
+    video.currentTime = 0;
+  });
+});
